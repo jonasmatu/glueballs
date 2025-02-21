3451ed0ac2f0eeb92122eb260f71f435b52e456a
Jonas Matuszak <jonas.matuszak@kit.edu>
2025-02-21 16:24:22 +0100

    Added nucleation calculation routine

---
+++ calc_nucleation.py
@@ -0,0 +1,156 @@
+"""Module to calculate the nucleation temperature
+
+Author: Jonas Matuszak <jonas.matuszak@kit.edu>, 2025
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+from calcBounce import *
+from potential import Potential
+
+from scipy import optimize
+
+
+def getActionAtT(T: float, pot: Potential, ndim: int, debug=False) -> (float, float):
+    """description
+
+    Parameters
+    ----------
+
+    Returns
+    ----------
+    S : float
+    phi0 : float
+    """
+
+    V = lambda x: pot.Vfull(x, T)
+    dV = lambda x: pot.dVfull(x, T)
+    d2V = lambda x: pot.d2Vfull(x, T)
+    
+    phibar = optimize.minimize_scalar(lambda phi: -V(phi), bounds=(0, pot.xmin),
+                                      options={"xatol": 1e-20}).x
+    phimeta = optimize.minimize_scalar(V, bounds=(-T, phibar),
+                                       options={"xatol": 1e-20}).x
+    # Sometimes only finds local minimum
+    if V(phimeta) > V(-T):
+        phimeta = -T
+    phiroot = optimize.brentq(lambda phi: V(phi) - V(phimeta), phibar, pot.xmin-1e-15)
+    if debug:
+        print("phiroot = ", phiroot)
+        print("V(phiroot) - V(-T) = ", V(phiroot)- V(-T))
+        print("phibar = ", phibar)
+        print("V(phibar)", V(phibar))
+        print("phimeta = ", phimeta)
+        print("V(phimeta)", V(phimeta))
+        print("at phibar = ", V(phibar) - V(phimeta))
+        print("at xmin = ", V(xmin) - V(phimeta))
+        pot.plotPotentialAll(phiroot*1.1, T, -T)
+        
+    
+
+    # print("phibar = ", phibar)
+    # print("phiroot = ", phiroot)
+
+    insta = Instanton(V, dV, d2V, ndim=ndim, Nkin=3*N**2/(2*np.pi**2),
+                      xtol=1e-15, phitol=1e-12, rmin_scaling=1e-8) # This setting is very important for small T!
+    r, y, ctype = insta.findProfile(phimeta, pot.xmin, phibar, f=1e-3)
+    phi, dphi = y.T
+    phi0 = phi[0]
+    S = insta.calcAction(r, phi, dphi, phimeta)
+    if debug:
+        print("ctype = ", ctype)
+        print(f"S = {S:2.5g}")
+        print(f"phi0 = {phi0:2.5g}")
+        fig, ax = plt.subplots(1,1,layout="constrained")
+        plt.semilogx(r, phi)
+        plt.xlabel("r")
+        plt.ylabel(r"$\phi$")
+        plt.show()
+
+    return S, phi0
+
+
+
+def nuclCriterion(S: float, phi0: float, T: float, xmin: float) -> float:
+    """Nucleation criterion
+
+    TODO: improve
+
+    Parameters
+    ----------
+    
+    Returns
+    ----------
+
+    """
+    Mpl_GeV = 1.220910e19
+    crit = S/(4 * np.log(phi0 * Mpl_GeV / xmin**2))
+
+    return crit - 1
+
+
+def findNucleationTemp(pot, Tmax: float, Tmin: float, ndim: int,
+                       Ttol=1e-2) -> float:
+    """description
+
+    Parameters
+    ----------
+
+    Returns
+    ----------
+
+    """
+    critdict = {}
+    def crit(T, critdict=critdict):
+        try:
+            T = T[0]
+        except:
+            pass
+        S, phi0 = getActionAtT(T, pot, ndim)
+        if ndim == 3:
+            S = S/T
+        critdict[T] = (S, phi0)
+        c = nuclCriterion(S, phi0, T, pot.xmin)
+        print(f"T = {T:2.5g}, S = {S:2.5g}, phi0 = {phi0:2.5g}, criterion = {c:2.5g}")
+        return c
+
+    def abort_fmin(T, critdict=critdict):
+        T = T[0]
+        S, phi0 = critdict[T]
+        if nuclCriterion(S, phi0, T, pot.xmin) < 0:
+            raise StopIteration(T)
+        elif T < 0:
+            raise StopIteration(T)
+
+    # First find the minimal value of the tunneling criterion!
+    try:
+        res = optimize.minimize(crit, 0.1*(Tmin+Tmax), method='Nelder-Mead', bounds=[(Tmin, Tmax)],
+                                 tol=.1, callback=abort_fmin)
+        Tmin = res.x[0]
+    except StopIteration as e:
+        Tmin = e.args[0]
+
+            
+    
+    print("Criterion at Tmin = ", nuclCriterion(critdict[Tmin][0], critdict[Tmin][1], Tmin, pot.xmin))
+    if nuclCriterion(critdict[Tmin][0], critdict[Tmin][1], Tmin, pot.xmin) > 0:
+        print("No tunneling possible, nucleation criterion not fulfulled!")
+        return -1
+
+    Tnuc = optimize.brentq(crit, Tmin, Tmax)
+    return Tnuc
+    
+
+
+if __name__=="__main__":
+    xmin = 2.5e3
+    vir = 1
+    eps = 1/20
+    delta = -.5
+    N = 4.5
+    withQCD=True
+    n = 0.15
+    ndim=3
+    
+    pot = Potential(xmin, vir, eps, delta, n, N=N, withQCD=withQCD)
+    Tnuc = findNucleationTemp(pot, 100, 1e-10, ndim, Ttol=1e-2)
--- test_QCDeffect.py
+++ test_QCDeffect.py
@@ -7,6 +7,7 @@ from potential import Potential
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import multiprocessing
 
 from scipy import optimize
@@ -31,18 +32,23 @@ def getActionAtT(T, xmin, vir, eps, delta, n, N, withQCD, ndim, debug=False):
     d2V = lambda x: pot.d2Vfull(x, T)
     
     phibar = optimize.minimize_scalar(lambda phi: -V(phi), bounds=(0, xmin),
-                                      options={"xatol": 1e-15}).x
+                                      options={"xatol": 1e-20}).x
     phimeta = optimize.minimize_scalar(V, bounds=(-T, phibar),
-                                       options={"xatol": 1e-15}).x
+                                       options={"xatol": 1e-20}).x
+    # Sometimes only finds local minimum
+    if V(phimeta) > V(-T):
+        phimeta = -T
     phiroot = optimize.brentq(lambda phi: V(phi) - V(phimeta), phibar, xmin-1e-15)
     if debug:
+        print("phiroot = ", phiroot)
+        print("V(phiroot) - V(-T) = ", V(phiroot)- V(-T))
         print("phibar = ", phibar)
         print("V(phibar)", V(phibar))
         print("phimeta = ", phimeta)
         print("V(phimeta)", V(phimeta))
         print("at phibar = ", V(phibar) - V(phimeta))
         print("at xmin = ", V(xmin) - V(phimeta))
-        pot.plotPotentialAll(phiroot*1.1, T, phimeta)
+        pot.plotPotentialAll(phiroot*1.1, T, -T)
         
     
 
@@ -50,24 +56,28 @@ def getActionAtT(T, xmin, vir, eps, delta, n, N, withQCD, ndim, debug=False):
     # print("phiroot = ", phiroot)
 
     insta = Instanton(V, dV, d2V, ndim=ndim, Nkin=3*N**2/(2*np.pi**2),
-                      xtol=1e-15, phitol=1e-12) # This setting is very important for small T!
-    r, y, ctype = insta.findProfile(phimeta, xmin, phibar)
+                      xtol=1e-15, phitol=1e-12, rmin_scaling=1e-8) # This setting is very important for small T!
+    r, y, ctype = insta.findProfile(phimeta, xmin, phibar, f=1e-3)
     phi, dphi = y.T
     phi0 = phi[0]
-
+    S = insta.calcAction(r, phi, dphi, phimeta)
     if debug:
+        print("ctype = ", ctype)
+        print(f"S = {S:2.5g}")
+        print(f"phi0 = {phi0:2.5g}")
+        fig, ax = plt.subplots(1,1,layout="constrained")
         plt.semilogx(r, phi)
         plt.xlabel("r")
         plt.ylabel(r"$\phi$")
         plt.show()
     
-    S = insta.calcAction(r, phi, dphi, phimeta)
+    
     return S, phi0
 
 
 
 
-def scan_n_at_T(T, fname, npoints=50):
+def scan_n_at_T(T, fname, nrange=(0.1,0.7), npoints=50):
     """description
 
     Parameters
@@ -84,8 +94,8 @@ def scan_n_at_T(T, fname, npoints=50):
     delta = -.5
     N = 4.5
 
-    
-    nrange = np.logspace(np.log10(0.1), np.log10(0.7), npoints)
+    # nrange = np.logspace(np.log10(0.1), np.log10(0.7), npoints)
+    nrange = np.linspace(nrange[0], nrange[1], npoints)
     S4range = np.zeros_like(nrange)
     S3range = np.zeros_like(nrange)
     for i, n in enumerate(nrange):
@@ -104,6 +114,102 @@ def scan_n_at_T(T, fname, npoints=50):
     np.savetxt(fname, np.asarray([nrange, S4range, S3range]), header=header, delimiter=",")
 
 
+def debug_n_at_T(T, fname, nrange=(0.1,0.7), npoints=50):
+    """description
+
+    Parameters
+    ----------
+
+    Returns
+    ----------
+
+    """
+
+    xmin = 2.5e3
+    vir = 1
+    eps = 1/20
+    delta = -.5
+    N = 4.5
+    withQCD=True
+    ndim=4
+
+    # nrange = np.logspace(np.log10(0.1), np.log10(0.7), npoints)
+    nrange = np.linspace(nrange[0], nrange[1], npoints)
+    S4range = np.zeros_like(nrange)
+    S3range = np.zeros_like(nrange)
+
+
+    rrange = []
+    phirange = []
+    Vrange = []
+    Vphirange = []
+    Srange = []
+    phimeta_range = []
+    
+    for i, n in enumerate(nrange):
+        try:
+            pot = Potential(xmin, vir, eps, delta, n, N=N, withQCD=withQCD)
+
+            V = lambda x: pot.Vfull(x, T)
+            dV = lambda x: pot.dVfull(x, T)
+            d2V = lambda x: pot.d2Vfull(x, T)
+    
+            phibar = optimize.minimize_scalar(lambda phi: -V(phi), bounds=(0, xmin),
+                                      options={"xatol": 1e-15}).x
+            phimeta = optimize.minimize_scalar(V, bounds=(-T*1.01, phibar),
+                                       options={"xatol": 1e-15}).x
+            if V(phimeta) > V(-T):
+                phimeta = -T
+            phimeta_range.append(phimeta)
+            phiroot = optimize.brentq(lambda phi: V(phi) - V(phimeta), phibar, xmin-1e-15)
+                
+            # print("V(phibar)", V(phibar))
+            # print("phimeta = ", phimeta)
+            # print("V(phimeta)", V(phimeta))
+            # print("at phibar = ", V(phibar) - V(phimeta))
+            # print("at xmin = ", V(xmin) - V(phimeta))
+            # pot.plotPotentialAll(phiroot*1.1, T, phimeta)
+            insta = Instanton(V, dV, d2V, ndim=ndim, Nkin=3*N**2/(2*np.pi**2),
+                              xtol=1e-15, phitol=1e-12, rmin_scaling=1e-6) # This setting is very important for small T!
+            r, y, ctype = insta.findProfile(phimeta, xmin, phibar)
+            phi, dphi = y.T
+            phi0 = phi[0]
+            S = insta.calcAction(r, phi, dphi, phimeta)
+            phirange.append(phi)
+            rrange.append(r)
+            Srange.append(S)
+            Vphirange.append(np.linspace(-T, phiroot, 100))
+            Vrange.append(V(Vphirange[i]))
+
+            # plt.semilogx(r, phi, color=cols[i])
+
+        except Exception as e:
+            print(e)
+            S4range[i] = np.nan
+
+    cmap = plt.get_cmap("plasma")
+    cols = cmap.resampled(256)
+    cols = cmap(np.linspace(0, 1, npoints))
+
+
+    fig, ax = plt.subplots(1,3, layout="constrained")
+    for i in range(len(rrange)):
+        ax[0].plot(nrange[i], Srange[i], 'o', color=cols[i])
+        ax[1].axhline(phimeta_range[i], color=cols[i], alpha=0.5)
+        ax[1].semilogx(rrange[i], phirange[i], color=cols[i])
+        ax[2].plot(Vphirange[i], Vrange[i], color=cols[i])
+    norm = mpl.colors.Normalize(vmin=nrange[0], vmax=nrange[-1])
+    clb = plt.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap), ax=ax)
+    clb.ax.set_ylabel(r"$n$")
+    ax[0].set_xlabel(r"$n$")
+    ax[0].set_ylabel(r"$S_4$")
+    ax[1].set_xlabel(r"$r$")
+    ax[1].set_ylabel(r"$\phi$")
+    ax[2].set_xlabel(r"$\phi$")
+    ax[2].set_ylabel(r"$V(\phi, T)$")
+    plt.show()
+
+    
 
 def plot_n_scan(fname):
     xmin = 0
